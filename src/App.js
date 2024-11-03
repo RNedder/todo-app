ee6108718aac57d5a3e41418200e98b0c4356fb1
Rose Nedder <rose.nedder@gmail.com>
2024-11-03 09:56:27 -0500

    installed react redux - added structure to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>todo-app</h1>
+      {/* list below for css testing purposes */}
+      <ul>
+        <li>task 1</li> 
+        <li>task 2</li>
+        <li>task 3</li>
+        <li>task 4</li>
+        <li>task 5</li>
+      </ul>
     </div>
   );
 }
