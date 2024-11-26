94742f27fe3a9650a6ab6b297f85e6a036c7a39b
Rose Nedder <rose.nedder@gmail.com>
2024-11-26 11:33:06 -0500

    increased max. chars to 150

--- src/utils/validateTaskForm.js
+++ src/utils/validateTaskForm.js
@@ -4,8 +4,8 @@ export const validateTaskForm = (values) => {
     // Task Validation
     if (!values.newTask) {
         errors.newTask = 'Enter a task';
-    } else if (values.newTask.length > 100) {
-        errors.newTask = 'Must be 100 characters or less';
+    } else if (values.newTask.length > 150) {
+        errors.newTask = 'Must be 150 characters or less';
     }
     // TODO - check if task already exists
 
